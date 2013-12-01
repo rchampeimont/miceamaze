63ee37f33457e336180d6dd3737cda695376dc45
Raphaël Champeimont <almacha@almacha.org>
2013-12-01 13:07:03 +0100

    small change in makeZIP.bat

--- project/makeZIP.bat
+++ project/makeZIP.bat
@@ -6,11 +6,9 @@ rd /S /Q MiceAmaze
 
 md MiceAmaze
 copy Release\*.dll MiceAmaze
+copy Release\*.exe MiceAmaze
 copy LICENSE.txt MiceAmaze
 copy ChangeLog.txt MiceAmaze
-copy Release\*.exe MiceAmaze
 xcopy rundir\*.* MiceAmaze /E
 ..\7-Zip\7za a MiceAmaze_RC.zip MiceAmaze
-
-rem rd /S /Q MiceAmaze
 
