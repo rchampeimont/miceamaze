6efd4e4efd4f06b937c3b41edec78db2ae0b90a4
Raphael Champeimont <almacha@almacha.org>
2015-02-16 15:51:06 +0100

    clean useless files

--- make-packages.sh
+++ make-packages.sh
@@ -10,6 +10,7 @@ echo "==== Making ${OUTDIR}.tar.gz ===="
 rm -rf $OUTDIR ${OUTDIR}.tar.gz
 make -C linux-project clean || exit 1
 cp -RL linux-project $OUTDIR || exit 1
+rm -rf $OUTDIR/{.settings,.cproject,.project}
 tar cfz ${OUTDIR}.tar.gz $OUTDIR || exit 1
 rm -rf $OUTDIR
 echo ""
