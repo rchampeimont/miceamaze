4ca3166b41c9281bcad05d7e479c1b54e9048fe9
Almacha <almacha@debian>
2015-02-16 14:13:38 +0100

    simplify

--- packaging/prepare.sh
+++ packaging/prepare.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#rm -f miceamaze-latest.tar.gz
-#wget http://www.miceamaze.org/source-packages/miceamaze-latest.tar.gz
-#rm -rf temp
-#mkdir temp
-#cd temp || exit 1
-#tar zxvf ../miceamaze-latest.tar.gz
-#VERSION=`grep -o "[0-9\\.]*" miceamaze-*/src/Version.h`
-#cp ../miceamaze-latest.tar.gz ../miceamaze_${VERSION}.orig.tar.gz
-
 uscan --destdir . --verbose --force-download
